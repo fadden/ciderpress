c2e3fb8353c3ad0b1798f1249c765578f8e3afd6
Andy McFadden <thefadden@gmail.com>
2017-09-22 17:40:23 -0700

    Update version for v4.0.2 release

--- app/MyApp.h
+++ app/MyApp.h
@@ -15,7 +15,7 @@
 #define kAppMajorVersion    4
 #define kAppMinorVersion    0
 #define kAppBugVersion      2
-#define kAppDevString       L"-a3"
+#define kAppDevString       L""
 
 /*
  * Windows application object.
