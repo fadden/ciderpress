21151cd76a95f36ec587aae4ae1e73e25187a713
Andy McFadden <fadden@fadden.com>
2020-07-12 11:10:44 -0700

    Version 4.0.4-d2

--- app/MyApp.h
+++ app/MyApp.h
@@ -15,7 +15,7 @@
 #define kAppMajorVersion    4
 #define kAppMinorVersion    0
 #define kAppBugVersion      4
-#define kAppDevString       L"d1"
+#define kAppDevString       L"d2"
 
 /*
  * Windows application object.
